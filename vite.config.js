312447c43cc8f5ec2a4de79a2d5616dc24343eaf
Alejandro Sarmiento <grancanash@gmail.com>
2025-09-13 20:21:34 +0100

    Nueva publicación

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './', // rutas relativas
-})
+    // Aquí puedes añadir tus configuraciones de Vite
+    // Por ejemplo, para el problema de la detección de cambios:
+    server: {
+        watch: {
+            usePolling: true // Habilita el "polling"
+        }
+    },
+    // O por ejemplo, para usar rutas relativas en los enlaces generados en el HTML (Te evitas problemas de rutas al desplegar en un servidor)
+    base: './',
+});
